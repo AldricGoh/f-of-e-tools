2b37aa6ebcc6a6b40c14a12fd0e9e6a66d7ddd43
Tom Grant <tg443@cam.ac.uk>
2022-05-25 21:02:57 +0000

    Fix bugged software blink

--- verilog/hardware/processor/source/softwareblink/softwareblink.c
+++ verilog/hardware/processor/source/softwareblink/softwareblink.c
@@ -1,8 +1,17 @@
-#include "softwareblink.h"
-
-int
-main(void)
+int main(void)
 {
+	enum
+	{
+		kSpinDelay = 400000,
+	};
+
+	/*
+	 *	Reading from the special address 0x2000 will cause sail to
+	 *	set the value of 8 of the FPGA's pins to the byte written
+	 *	to the address. See the PCF file for how those 8 pins are
+	 *	mapped.
+	 */
+	volatile unsigned int *gDebugLedsMemoryMappedRegister = (unsigned int *)0x2000;
 	/*
 	 *	Reading from the special address pointed to by
 	 *	kDebugLedsMemoryMappedRegister will cause the processor to
@@ -10,20 +19,22 @@ main(void)
 	 *	to the address. See the PCF file for how those 8 pins are
 	 *	mapped.
 	 */
-	while(1)
+	while (1)
 	{
 		*gDebugLedsMemoryMappedRegister = 0xFF;
 
 		/*
 		 *	Spin
 		 */
-		for (int j = 0; j < kSpinDelay; j++);
+		for (int j = 0; j < kSpinDelay; j++)
+			;
 
 		*gDebugLedsMemoryMappedRegister = 0x00;
 
 		/*
 		 *	Spin
 		 */
-		for (int j = 0; j < kSpinDelay; j++);		
+		for (int j = 0; j < kSpinDelay; j++)
+			;
 	}
 }
