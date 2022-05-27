13c2a53f972b4f94974fd221b1d3582f1dc4bf46
Akash <ag2118@cam.ac.uk>
2022-05-27 11:01:58 +0000

    reset branch predictor. modified adder

--- verilog/hardware/processor/sail-core/verilog/adder.v
+++ verilog/hardware/processor/sail-core/verilog/adder.v
@@ -45,10 +45,70 @@
 
 
 
-module adder(input1, input2, out);
-	input [31:0]	input1;
-	input [31:0]	input2;
-	output [31:0]	out;
+// module adder(input1, input2, out);
+// 	input [31:0]	input1;
+// 	input [31:0]	input2;
+// 	output [31:0]	out;
 
-	assign		out = input1 + input2;
+// 	assign		out = input1 + input2;
+// endmodule
+
+
+module adder(input1, input2, out);
+  input [31:0] input1;
+  input [31:0] input2;
+  output [31:0] out;
+  // wire [31 : 0] alu_output;
+	SB_MAC16 i_sbmac16
+	( // port interfaces
+		.A(input1[31 : 16]),
+		.B(input1[15 : 0]),  // A[31 : 16]
+		.C(input2[31 : 16]),
+		.D(input2[15 : 0]),
+		.O(out),
+		.CLK(), // HERE
+		.CE(1'b0),
+		.IRSTTOP(1'b0),
+		.IRSTBOT(1'b0),
+		.ORSTTOP(1'b0),
+		.ORSTBOT(1'b0),
+		.AHOLD(1'b0),
+		.BHOLD(1'b0),
+		.CHOLD(1'b0),
+		.DHOLD(1'b0),
+		.OHOLDTOP(1'b0),
+		.OHOLDBOT(1'b0),
+		.OLOADTOP(1'b0),
+		.OLOADBOT(1'b0),
+		.ADDSUBTOP(1'b0),
+		.ADDSUBBOT(1'b0),
+		.CO(),
+		.CI(1'b0),
+		.ACCUMCI(1'b0),
+		.ACCUMCO(),
+		.SIGNEXTIN(1'b0),
+		.SIGNEXTOUT()
+	);
+	defparam i_sbmac16.NEG_TRIGGER = 1'b0;
+	defparam i_sbmac16.C_REG = 1'b0;
+	defparam i_sbmac16.A_REG = 1'b0;
+	defparam i_sbmac16.B_REG = 1'b0;
+	defparam i_sbmac16.D_REG = 1'b0;
+
+	defparam i_sbmac16.TOP_8x8_MULT_REG = 1'b0;
+	defparam i_sbmac16.BOT_8x8_MULT_REG = 1'b0;
+	defparam i_sbmac16.PIPELINE_16x16_MULT_REG1 = 1'b0;
+	defparam i_sbmac16.PIPELINE_16x16_MULT_REG2 = 1'b0;
+
+	defparam i_sbmac16.TOPOUTPUT_SELECT = 2'b00; // accum register output at O[31:16]
+	defparam i_sbmac16.TOPADDSUB_LOWERINPUT = 2'b00;
+	defparam i_sbmac16.TOPADDSUB_UPPERINPUT = 1'b1;
+	defparam i_sbmac16.TOPADDSUB_CARRYSELECT = 2'b10;
+	defparam i_sbmac16.BOTOUTPUT_SELECT = 2'b00; // accum regsiter output at O[15:0]
+	defparam i_sbmac16.BOTADDSUB_LOWERINPUT = 2'b00;
+	defparam i_sbmac16.BOTADDSUB_UPPERINPUT = 1'b1;
+	defparam i_sbmac16.BOTADDSUB_CARRYSELECT = 2'b00;
+	defparam i_sbmac16.MODE_8x8 = 1'b1;
+	defparam i_sbmac16.A_SIGNED = 1'b0;
+	defparam i_sbmac16.B_SIGNED = 1'b0;
 endmodule
--- verilog/hardware/processor/sail-core/verilog/branch_predictor.v
+++ verilog/hardware/processor/sail-core/verilog/branch_predictor.v
@@ -70,14 +70,9 @@ module branch_predictor(
 	/*
 	 *	internal state
 	 */
+	reg [1:0]	s;
+
 	reg		branch_mem_sig_reg;
-
-	reg [1:0] state[0: 2**6 - 1];
-
-	wire [6:0] current_addr_hash;
-
-	reg [6:0] last_addr_hash;
-	reg [6:0] second_last_addr_hash;
 
 	/*
 	 *	The `initial` statement below uses Yosys's support for nonzero
@@ -89,15 +84,13 @@ module branch_predictor(
 	 *	the design should instead use a reset signal going to
 	 *	modules in the design and to thereby set the values.
 	 */
-	integer i;
 	initial begin
-		for (i=0; i<2**6; i=i+1) state[i] = 2'b00;
+		s = 2'b00;
 		branch_mem_sig_reg = 1'b0;
 	end
 
 	always @(negedge clk) begin
 		branch_mem_sig_reg <= branch_mem_sig;
-		last_addr <= current_addr_hash;
 	end
 
 	/*
@@ -107,14 +100,11 @@ module branch_predictor(
 	 */
 	always @(posedge clk) begin
 		if (branch_mem_sig_reg) begin
-			state[second_last_addr_hash][1] <= (state[second_last_addr_hash][1]&state[second_last_addr_hash][0]) | (state[second_last_addr_hash][0]&actual_branch_decision) | (state[second_last_addr_hash][1]&actual_branch_decision);
-			state[second_last_addr_hash][0] <= (state[second_last_addr_hash][1]&(!state[second_last_addr_hash][0])) | ((!state[second_last_addr_hash][0])&actual_branch_decision) | (state[second_last_addr_hash][1]&actual_branch_decision);
+			s[1] <= (s[1]&s[0]) | (s[0]&actual_branch_decision) | (s[1]&actual_branch_decision);
+			s[0] <= (s[1]&(!s[0])) | ((!s[0])&actual_branch_decision) | (s[1]&actual_branch_decision);
 		end
-		second_last_addr_hash <= last_addr_hash;
-		last_addr_hash <= current_addr_hash;
 	end
 
 	assign branch_addr = in_addr + offset;
-	assign current_addr_hash = in_addr[8:2];
-	assign prediction = state[current_addr_hash][1] & branch_decode_sig;
+	assign prediction = s[1] & branch_decode_sig;
 endmodule
