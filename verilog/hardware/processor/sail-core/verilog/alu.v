a074cb878080585ef2dd9cf1f524eab7fa9afdb2
Tom Grant <tg443@cam.ac.uk>
2022-05-27 09:20:38 +0000

    Hardware ALU

--- verilog/hardware/processor/sail-core/verilog/alu.v
+++ verilog/hardware/processor/sail-core/verilog/alu.v
@@ -61,6 +61,62 @@ module alu(ALUctl, A, B, ALUOut, Branch_Enable);
 	output reg [31:0]	ALUOut;
 	output reg		Branch_Enable;
 
+	wire [31 : 0] alu_output;
+	SB_MAC16 i_sbmac16
+	( // port interfaces
+		.A(A[31 : 16]),
+		.B(A[15 : 0]),  // A[31 : 16]
+		.C(B[31 : 16]),
+		.D(B[15 : 0]),
+		.O(alu_output),
+		.CLK(), // HERE
+		.CE(1'b0),
+		.IRSTTOP(1'b0),
+		.IRSTBOT(1'b0),
+		.ORSTTOP(1'b0),
+		.ORSTBOT(1'b0),
+		.AHOLD(1'b0),
+		.BHOLD(1'b0),
+		.CHOLD(1'b0),
+		.DHOLD(1'b0),
+		.OHOLDTOP(1'b0),
+		.OHOLDBOT(1'b0),
+		.OLOADTOP(1'b0),
+		.OLOADBOT(1'b0),
+		.ADDSUBTOP(1'b0),
+		.ADDSUBBOT(1'b0),
+		.CO(),
+		.CI(1'b0),
+		.ACCUMCI(1'b0),
+		.ACCUMCO(),
+		.SIGNEXTIN(1'b0),
+		.SIGNEXTOUT()
+	);
+	defparam i_sbmac16.NEG_TRIGGER = 1'b0;
+	defparam i_sbmac16.C_REG = 1'b0;
+	defparam i_sbmac16.A_REG = 1'b0;
+	defparam i_sbmac16.B_REG = 1'b0;
+	defparam i_sbmac16.D_REG = 1'b0;
+
+	defparam i_sbmac16.TOP_8x8_MULT_REG = 1'b0;
+	defparam i_sbmac16.BOT_8x8_MULT_REG = 1'b0;
+	defparam i_sbmac16.PIPELINE_16x16_MULT_REG1 = 1'b0;
+	defparam i_sbmac16.PIPELINE_16x16_MULT_REG2 = 1'b0;
+
+	defparam i_sbmac16.TOPOUTPUT_SELECT = 2'b00; // accum register output at O[31:16]
+	defparam i_sbmac16.TOPADDSUB_LOWERINPUT = 2'b00;
+	defparam i_sbmac16.TOPADDSUB_UPPERINPUT = 1'b1;
+	defparam i_sbmac16.TOPADDSUB_CARRYSELECT = 2'b10;
+	defparam i_sbmac16.BOTOUTPUT_SELECT = 2'b00; // accum regsiter output at O[15:0]
+	defparam i_sbmac16.BOTADDSUB_LOWERINPUT = 2'b00;
+	defparam i_sbmac16.BOTADDSUB_UPPERINPUT = 1'b1;
+	defparam i_sbmac16.BOTADDSUB_CARRYSELECT = 2'b00;
+	defparam i_sbmac16.MODE_8x8 = 1'b1;
+	defparam i_sbmac16.A_SIGNED = 1'b0;
+	defparam i_sbmac16.B_SIGNED = 1'b0;
+	//defparam i_sbmac16.BOTOUTPUT_SELECT = 2'b01 ;// accum regsiter output at O[15:0].
+	//defparam i_sbmac16.TOPOUTPUT_SELECT = 2'b01 ;// accum register output at O[31:16]
+
 	/*
 	 *	This uses Yosys's support for nonzero initial values:
 	 *
@@ -90,7 +146,7 @@ module alu(ALUctl, A, B, ALUOut, Branch_Enable);
 			/*
 			 *	ADD (the fields also match AUIPC, all loads, all stores, and ADDI)
 			 */
-			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_ADD:	ALUOut = A + B;
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_ADD:	ALUOut = alu_output;
 
 			/*
 			 *	SUBTRACT (the fields also matches all branches)
