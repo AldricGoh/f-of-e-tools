03100bf18095ff9959c58c15f99677910f46474d
Tom Grant <thomas.grant.mail@gmail.com>
2022-06-07 16:10:21 +0100

    Remove everything except branch prediction

--- verilog/hardware/processor/make_for_iverilog.sh
+++ verilog/hardware/processor/make_for_iverilog.sh
@@ -13,7 +13,7 @@ cp programs/program.hex verilog/
 cd sail-core/verilog
 echo `pwd`
 
-verilator -DSIMULATION --lint-only --clk clk -cc clk_gate.v adder.v cpu.v mux2to1.v alu_control.v pipeline_registers.v alu.v program_counter.v branch_decide.v forwarding_unit.v branch_predictor.v imm_gen.v control_unit.v instruction_mem.v data_mem.v register_file.v CSR.v dataMem_mask_gen.v ../../toplevel.v --exe main.cpp
-iverilog -DSIMULATION -DIVERILOG -o ../../iv.out clk_gate.v SB_HFOCSC.v adder.v cpu.v mux2to1.v alu_control.v pipeline_registers.v alu.v program_counter.v branch_decide.v forwarding_unit.v branch_predictor.v imm_gen.v control_unit.v instruction_mem.v data_mem.v register_file.v CSR.v dataMem_mask_gen.v ../../toplevel.v
+verilator -DSIMULATION --lint-only --clk clk -cc adder.v cpu.v mux2to1.v alu_control.v pipeline_registers.v alu.v program_counter.v branch_decide.v forwarding_unit.v branch_predictor.v imm_gen.v control_unit.v instruction_mem.v data_mem.v register_file.v CSR.v dataMem_mask_gen.v ../../toplevel.v --exe main.cpp
+iverilog -DSIMULATION -DIVERILOG -o ../../iv.out SB_HFOCSC.v adder.v cpu.v mux2to1.v alu_control.v pipeline_registers.v alu.v program_counter.v branch_decide.v forwarding_unit.v branch_predictor.v imm_gen.v control_unit.v instruction_mem.v data_mem.v register_file.v CSR.v dataMem_mask_gen.v ../../toplevel.v
 
 echo "Output iv.out created"
