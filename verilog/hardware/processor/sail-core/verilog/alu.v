5077631d672f77975d739c1605286b342a279996
Akash <ag2118@cam.ac.uk>
2022-06-08 19:47:09 +0000

    dsp add, sub, unsigned compare.

--- verilog/hardware/processor/sail-core/verilog/alu.v
+++ verilog/hardware/processor/sail-core/verilog/alu.v
@@ -61,6 +61,29 @@ module alu(ALUctl, A, B, ALUOut, Branch_Enable);
 	output reg [31:0]	ALUOut;
 	output reg		Branch_Enable;
 
+  // DSP wires
+  wire [31:0] add_out;
+  wire [31:0] sub_out;
+  wire carry_out;
+
+  // DSP add
+  dsp_add async32add(
+    .input1(A),
+    .input2(B),
+    // .add_sub(add_sub),
+    .out(add_out),
+    // .carry_out(carry_out)
+  );
+
+  // DSP Subtract
+  dsp_sub async32add_sub(
+  .input1(A),
+  .input2(B),
+  // .add_sub(add_sub),
+  .out(sub_out),
+  .carry_out(carry_out)
+  );
+
 	initial begin
 		ALUOut = 32'b0;
 		Branch_Enable = 1'b0;
@@ -81,12 +104,12 @@ module alu(ALUctl, A, B, ALUOut, Branch_Enable);
 			/*
 			 *	ADD (the fields also match AUIPC, all loads, all stores, and ADDI)
 			 */
-			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_ADD:	ALUOut = A + B;
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_ADD:	ALUOut = add_out; // A + B;
 
 			/*
 			 *	SUBTRACT (the fields also matches all branches)
 			 */
-			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SUB:	ALUOut = A - B;
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SUB:	ALUOut = sub_out; // A - B;
 
 			/*
 			 *	SLT (the fields also matches all the other SLT variants)
@@ -141,8 +164,8 @@ module alu(ALUctl, A, B, ALUOut, Branch_Enable);
 			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BNE:	Branch_Enable = !(ALUOut == 0);
 			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BLT:	Branch_Enable = ($signed(A) < $signed(B));
 			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BGE:	Branch_Enable = ($signed(A) >= $signed(B));
-			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BLTU:	Branch_Enable = ($unsigned(A) < $unsigned(B));
-			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BGEU:	Branch_Enable = ($unsigned(A) >= $unsigned(B));
+			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BLTU:	Branch_Enable = (~carry_out); // ($unsigned(A) < $unsigned(B));
+			`kSAIL_MICROARCHITECTURE_ALUCTL_6to4_BGEU:	Branch_Enable = carry_out; // ($unsigned(A) >= $unsigned(B));
 
 			default:					Branch_Enable = 1'b0;
 		endcase
