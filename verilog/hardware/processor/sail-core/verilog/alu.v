e6085b33e5c0eea73afa8c5d4757c1d440cd9b6f
Akash <ag2118@cam.ac.uk>
2022-06-08 14:36:10 +0000

    corrected SLT output

--- verilog/hardware/processor/sail-core/verilog/alu.v
+++ verilog/hardware/processor/sail-core/verilog/alu.v
@@ -127,7 +127,7 @@ module alu(ALUctl, A, B, ALUOut, Branch_Enable);
 			/*
 			 *	SLT (the fields also matches all the other SLT variants)
 			 */
-			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SLT:	ALUOut = signed_Aless_thanB_out; // $signed(A) < $signed(B) ? 32'b1 : 32'b0;;
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SLT:	ALUOut = signed_Aless_thanB_out ? 32'b1 : 32'b0; // $signed(A) < $signed(B) ? 32'b1 : 32'b0;;
 
 			/*
 			 *	SRL (the fields also matches the other SRL variants)
